271796ebe4adf81c74a706f738e5be344a76947b
nezeek <nezikurd83@gmail.com>
2023-02-15 20:10:13 +0300

    Appbar created

---
+++ lib/nezik/newScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Nsceen extends StatefulWidget {
+  const Nsceen({super.key});
+
+  @override
+  State<Nsceen> createState() => NsceenState();
+}
+
+class NsceenState extends State<Nsceen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(),);
+  }
+}
