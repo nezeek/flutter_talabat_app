75266b640f0ca60eb5c3b15a32660f49e37f44c3
dlkhaz <dlkaz.jh7720@stu.uod.ac>
2023-03-14 15:00:47 +0300

    DlkhazUpdateAccount

--- lib/Dlkhaz/account.dart
+++ lib/Dlkhaz/account.dart
@@ -1,36 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter_talabat_app/Dlkhaz/ad.dart';
+
 
 class Account extends StatelessWidget {
   const Account({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Account",),
-    elevation: 10,
+    return Scaffold(appBar:
+     AppBar(
+      leading: IconButton(onPressed: (){}, icon: const Icon(Icons.account_circle)),
+      title:const Text("Account",),
+   actions: [
+    IconButton(onPressed: (){}, icon: Icon(Icons.settings))],
     backgroundColor: Colors.white,
-    foregroundColor: Colors.black,
-    leading: Container(
-      child: Icon(Icons.account_box),
-    ),
+    foregroundColor: Colors.black,),
     
-    ),
-    
-    body: ListView.builder(
-      itemCount: datalist.length,
-      itemBuilder: (context,index){
-        Data date=datalist[index];
-        return ListTile(
+
+    body: ListView(
+      children: [
+        ListTile(
+          leading: Icon(Icons.description),
+          title: Text("Your order"),
+
+        ),
+        ListTile(
+          leading: Icon(Icons.local_offer),
+          title: Text("Offers"),
+
+        ),
+           ListTile(
+          leading: Icon(Icons.notifications),
+          title: Text("Notifications"),
+
+        ),
+           ListTile(
+          leading: Icon(Icons.volcano_rounded),
+          title: Text("Vouchers"),
+
+        ),
+           ListTile(
+          leading: Icon(Icons.help_sharp),
+          title: Text("Get help"),
+
+        ),
+           ListTile(
+          leading: Icon(Icons.report_gmailerrorred),
+          title: Text("About app"),
+
+        ),
 
 
-title: Text(date.nam),
-leading: Icon(Icons.notifications),
 
-        );
 
-      }),
+
+
+
+
+      ],
+
+    )
     
     
     
