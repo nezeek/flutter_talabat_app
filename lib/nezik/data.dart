a3c9cffcb719cd661e6a0452467150e27f8f2980
nezeek <nezikurd83@gmail.com>
2023-03-20 20:36:27 +0300

    new file created

---
+++ lib/nezik/data.dart
@@ -0,0 +1,159 @@
+
+
+class Logo{
+  String title;
+  String imageurl;
+  String description;
+  String top;
+  String price;
+  String time;
+
+  Logo({
+    required this.title,
+    required this.description,
+    required this.imageurl,
+    required this.price,
+    required this.time,
+    required this.top
+  });
+}
+
+List <Logo> logolist=[
+
+Logo(
+  title: "KFC", 
+  description: "Fried Chicken,Fast Food,sandwiches", 
+  imageurl: "images/KFC.jpg", 
+  price: "IQD 2500", 
+  time: "48 mins",
+  top: "4.6 (100+)"),
+
+  Logo(
+  title: "LUNAR", 
+  description: "Pizza,sandwiches,Italian", 
+  imageurl: "images/lunar.jpg", 
+  price: "IQD 2500", 
+  time: "35 mins",
+  top: "3.5 (100+)"),
+
+  Logo(
+  title: "Marjan", 
+  description: "Pastries,Pizza,sandwiches", 
+  imageurl: "images/marjan.jpg", 
+  price: "Free", 
+  time: "32 mins",
+  top: "4.1 (100+)"),
+
+  Logo(
+  title: "Vikings", 
+  description: "Pizza,shawarma,grills", 
+  imageurl: "images/vikings.jpg", 
+  price: "IQD 2500", 
+  time: "39 mins",
+  top: "4.1 (100+)"),
+
+  Logo(
+  title: "Burger Box", 
+  description: "Burger,Fast Food,sandwiches", 
+  imageurl: "images/burgerbox.jpg", 
+  price: "IQD 2500", 
+  time: "37 mins",
+  top: "3.8 (100+)"),
+
+  Logo(
+  title: "Akre rest", 
+  description: "Pizza,,Italian", 
+  imageurl: "images/akre.jpg", 
+  price: "IQD 3000", 
+  time: "38 Min",
+  top: "3.7 (100+)"),
+
+  Logo(
+  title: "Burger King", 
+  description: "Fast Food,Burgers,sandwiches", 
+  imageurl: "images/BurgerKing.jpg", 
+  price: "IQD 3000", 
+  time: "55 mins",
+  top: "4.7 (100+)"),
+
+  Logo(
+  title: "Wienerwald", 
+  description: "Burgers,Pizza,Pasta", 
+  imageurl: "images/Wienerwald.jpg", 
+  price: "IQD 2500", 
+  time: "48 mins",
+  top: "4.0 (100+)"),
+  
+  Logo(
+  title: "Torino Pizza", 
+  description: "Pizza,sandwiches,Italian", 
+  imageurl: "images/toriino.jpg", 
+  price: "IQD 2500", 
+  time: "35 mins",
+  top: "4.5 (100+)"),
+
+  Logo(
+  title: "Mam U Zin", 
+  description: "sandwiches,Burgers,Cafe", 
+  imageurl: "images/mamuzin.jpg", 
+  price: "IQD 2500", 
+  time: "31 mins",
+  top: "4.0 (100+)"),
+
+  Logo(
+  title: "Norjan", 
+  description: "sandwiches,Chicken,Grills", 
+  imageurl: "images/norjan.jpg", 
+  price: "IQD 2500", 
+  time: "41 mins",
+  top: "4.6 (100+)"),
+
+  Logo(
+  title: "Ofelia", 
+  description: "Pasta,Burgers,Pizza", 
+  imageurl: "images/Ofelia.jpg", 
+  price: "IQD 2500", 
+  time: "41 mins",
+  top: "4.6 (100+)"),
+
+  Logo(
+  title: "2in1", 
+  description: "Pizza,Burgers,Italian", 
+  imageurl: "images/2in1.jpg", 
+  price: "IQD 2500", 
+  time: "41 mins",
+  top: "4.1 (100+)"),
+
+  Logo(
+  title: "Segafredo", 
+  description: "Burgers,International,Pasta", 
+  imageurl: "images/Segafredo.jpg", 
+  price: "IQD 2500", 
+  time: "52 mins",
+  top: "3.4 (100+)"),
+
+  Logo(
+  title: "MADO", 
+  description: "Pizza,sandwiches,Italian", 
+  imageurl: "images/mado.jpg", 
+  price: "IQD 2500", 
+  time: "42 mins",
+  top: "5.4 (100+)"),
+
+  Logo(
+  title: "DDK", 
+  description: "Pizza,sandwiches,Italian", 
+  imageurl: "images/DDK.jpg", 
+  price: "IQD 3000", 
+  time: "32 mins",
+  top: "5.5 (100+)"),
+
+  Logo(
+  title: "Burgerat", 
+  description: "burger,Chicken,American", 
+  imageurl: "images/burgerat.jpg", 
+  price: "Free", 
+  time: "37 mins",
+  top: "4.5 (100+)"),
+
+];
