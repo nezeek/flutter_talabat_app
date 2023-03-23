ac9268ea64a0fc1903403b453ce711158c220f3e
sara <sarah1kmat71@gmail.om>
2023-03-24 00:57:34 +0300

    data item change

--- lib/saraa folder/models/menu.dart
+++ lib/saraa folder/models/menu.dart
@@ -18,8 +18,8 @@ final List<CategoryMenu> demoCategoryMenus = [
   CategoryMenu(
     category: "Trending",
     items: [
-      Menu(title: "2 in 1 Cheese Burger",image: "https://th.bing.com/th/id/OIP.r88qg2oomrY6Ypgkx3pTYAHaFS?pid=ImgDet&w=700&h=500&rs=1", description: "Homemade burger bun", price: 9.500),
-      Menu(title: "Crazy Fries", image: "https://th.bing.com/th/id/R.94d0212a34ff4e2992af10684c361983?rik=Mr0IOSQdC5lqtw&riu=http%3a%2f%2fi.imgur.com%2fnR37mxn.jpg&ehk=CrtEjuqIVFxrTq71FIFk1SPQxC1Ao1ctnQSHYLTy4ck%3d&risl=&pid=ImgRaw&r=0", description: "french fries,chedder cheese",price: 5.500),
+      Menu(title: "Duble Cheese Burger", image: "https://www.desktopbackground.org/download/1280x1024/2010/06/04/28023_cheeseburger-hd-magnificent-wallpapers-free-hd-wallpapers-download_1920x1080_h.jpg", description: "2 black angus beef patty",price: 12.000),
+     Menu(title: "Chiken Burger", image: "https://th.bing.com/th/id/OIP.1iga5uTndtz8Yro5x-PTZwHaFh?w=257&h=191&c=7&r=0&o=5&pid=1.7", description: "crispy chiken patty,green salad",price: 8.500),
       Menu(title: "Curly Fries", image: "https://th.bing.com/th/id/OIP.bO2_Qdxj4d4ENlGuCbVNRgHaE7?w=261&h=180&c=7&r=0&o=5&pid=1.7", description: " ",price: 3.500),
       Menu(title: "Margherita pizza", image: "https://th.bing.com/th/id/OIP.8jZsScsCyLaXkVWldiDB1wHaHa?pid=ImgDet&w=640&h=640&rs=1", description: "mozzarela cheese,orango and tomato",price: 7.000),
     ],
@@ -34,21 +34,18 @@ final List<CategoryMenu> demoCategoryMenus = [
   CategoryMenu(
     category: "Burger",
     items: [
-           Menu(title: "2 in 1 Cheese Burger",image: "https://th.bing.com/th/id/OIP.r88qg2oomrY6Ypgkx3pTYAHaFS?pid=ImgDet&w=700&h=500&rs=1", description: "Homemade burger bun", price: 9500),
-      Menu(title: "Duble Cheese Burger", image: "https://th.bing.com/th/id/R.ab975d07311ae26d16aebdc9d9175e94?rik=Z%2fR6DLNZod7wOQ&riu=http%3a%2f%2f1.bp.blogspot.com%2f_ilXHL_b-fEY%2fTAagLShLQKI%2fAAAAAAAAAmI%2f_IHNv6C5pfI%2fs1600%2fdennys-double-cheeseburger.jpg&ehk=85wLDCMWcTrtsM%2bJy3gFT5%2bwNWz0kA4UwOqrFdtHP04%3d&risl=&pid=ImgRaw&r=0", description: "2 black angus beef patty",price: 12000),
+           Menu(title: "2 in 1 Cheese Burger",image: "https://th.bing.com/th/id/OIP.r88qg2oomrY6Ypgkx3pTYAHaFS?pid=ImgDet&w=700&h=500&rs=1", description: "Homemade burger bun", price: 9.500),
+      Menu(title: "Duble Cheese Burger", image: "https://www.desktopbackground.org/download/1280x1024/2010/06/04/28023_cheeseburger-hd-magnificent-wallpapers-free-hd-wallpapers-download_1920x1080_h.jpg", description: "2 black angus beef patty",price: 12.000),
       Menu(title: "Chiken Burger", image: "https://th.bing.com/th/id/OIP.1iga5uTndtz8Yro5x-PTZwHaFh?w=257&h=191&c=7&r=0&o=5&pid=1.7", description: "crispy chiken patty,green salad",price: 8.500),
       Menu(title: "Farm Burger", image: "https://th.bing.com/th/id/OIP.v5vr2EAdreKvqJtnZs2HJwHaE5?w=261&h=180&c=7&r=0&o=5&pid=1.7", description: "grilled egg",price: 10.500),
       Menu(title: "Vegetable Burger", image: "https://th.bing.com/th/id/OIP.Aq5Uy558k9Y91bnWqilNyAAAAA?pid=ImgDet&w=435&h=435&rs=1", description: "vegeterian patty",price: 6.500),
     ],
   ),
   CategoryMenu(
-    category: "kaskati",
+    category: "Kids Meals",
     items: [
-      Menu(title: "Cookie", image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", description: "Hello world Hello world",price: 7.4),
-      Menu(title: "Cookie", image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", description: "Hello world Hello world",price: 7.4),
-      Menu(title: "Cookie", image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", description: "Hello world Hello world",price: 7.4),
-      Menu(title: "Cookie", image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", description: "Hello world Hello world",price: 7.4),
-      Menu(title: "Cookie", image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", description: "Hello world Hello world",price: 7.4),
+      Menu(title: "Kids Burger Meal", image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", description: "served with french friese",price:6.500),
+      Menu(title: "Chicken Popcorn Meal", image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", description: "served french with drink",price: 6.500),
     ],
   ),
 ];
