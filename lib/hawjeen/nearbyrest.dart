5699f203f8c9120e436e1103910019603224ff9f
nezeek <nezikurd83@gmail.com>
2023-03-25 22:51:57 +0300

    nama changes

--- lib/hawjeen/nearbyrest.dart
+++ lib/hawjeen/nearbyrest.dart
@@ -6,12 +6,12 @@ class Nearby {
 }
 
 List<Nearby> nearb = [
-  Nearby(imag: "images/KFC.jpg", Nav: "2in1",description: "2.3 mins"),
-  Nearby(imag: "images/2in1.jpg", Nav: "88",description: "4.7 mins"),
-  Nearby(imag: "images/lunar.jpg", Nav: "Burgerking",description: "5.1 mins"),
-  Nearby(imag: "images/marjan.jpg", Nav: "DDk",description: "4.3 mins"),
-  Nearby(imag: "images/vikings.jpg", Nav: "Ofelia",description: "2.5 mins"),
-  Nearby(imag: "images/burgerbox.jpg", Nav: "Torino",description: "4.1 mins"),
-  Nearby(imag: "images/BurgerKing.jpg", Nav: "Shaghaf",description: "6.4 mins"),
-  Nearby(imag: "images/toriino.jpg", Nav: "BurgerBox",description: "5.5 mins")
+  Nearby(imag: "images/KFC.jpg", Nav: "KFC",description: "2.3 mins"),
+  Nearby(imag: "images/2in1.jpg", Nav: "2in1",description: "4.7 mins"),
+  Nearby(imag: "images/lunar.jpg", Nav: "Lunar",description: "5.1 mins"),
+  Nearby(imag: "images/marjan.jpg", Nav: "Marjan",description: "4.3 mins"),
+  Nearby(imag: "images/vikings.jpg", Nav: "Vikings",description: "2.5 mins"),
+  Nearby(imag: "images/burgerbox.jpg", Nav: "BurgarBox",description: "4.1 mins"),
+  Nearby(imag: "images/BurgerKing.jpg", Nav: "burgerking",description: "6.4 mins"),
+  Nearby(imag: "images/toriino.jpg", Nav: "torino",description: "5.5 mins")
 ];
