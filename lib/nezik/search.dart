b05751c1be67a5df51fda455d511caacb0109411
nezeek <nezikurd83@gmail.com>
2023-03-31 15:47:07 +0300

    search screen

--- lib/nezik/search.dart
+++ lib/nezik/search.dart
@@ -13,7 +13,8 @@ class search_file extends StatelessWidget {
         child:Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-          Column(children: [
+          Column(
+            children: [
             Padding(padding: EdgeInsets.symmetric(
         vertical: 25,),
         child:Container(
@@ -22,46 +23,46 @@ class search_file extends StatelessWidget {
                   icon: Icon(Icons.arrow_back),color: Colors.black,)
             ),
          ), ],),
-      
-      Column(children: [
-      Padding(padding: EdgeInsets.symmetric(
-        vertical: 25,
-        horizontal: 5),
-        
-      child:Container(
-        height: 50,
-        width: 330,
-        decoration: BoxDecoration(
+
+         Column(children: [
+            Padding(padding: EdgeInsets.symmetric(
+            vertical: 25,
+            horizontal: 5),
+          child:Container(
+          height: 50,
+          width: 330,
+          decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(8),
           border: Border.all(color: Colors.black26),
-        ),
-        child: Padding(padding: EdgeInsets.symmetric(
-          horizontal: 5,
         ),
         child: Row(
           children: [
-          Icon(CupertinoIcons.search,
-          color: Colors.grey,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(CupertinoIcons.search,
+              color: Colors.grey,
+              ),
+            ],
           ),
-          Container(
-            height: 50,
+          Column(children: [
+             Container(
             width: 300,
             child: Padding(padding: EdgeInsets.symmetric(
               horizontal: 13,
             ),
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: "Search for restaurants or dishes",
-              ),
-            ),
-            ),
-          ),
-        ],),),
-      ),
-      ),
-      ],),
-     ],), ),
+               child: TextFormField(
+                  decoration: InputDecoration(
+                  hintText: "Search for restaurants or dishes",
+                ),
+              ),),
+             ),
+          ]),
+          ]),
+        ),),
+         ],),
+     ]), ),
 
      
     );
